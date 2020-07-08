16d99d031664d25d222f31e69d6902231926dbea
Ugo THEVENY <ugtheven@e1r4p8.42.fr>
2020-07-08 11:37:44 +0200

    Modif print tabFunc

--- Sources/ft_printf.c
+++ Sources/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ugtheven <ugtheven@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 13:53:16 by ugtheven          #+#    #+#             */
-/*   Updated: 2020/07/08 11:32:51 by ugtheven         ###   ########.fr       */
+/*   Updated: 2020/07/08 11:36:46 by ugtheven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ int		ft_printf(const char *format, ...)
 	void (*tabFunc[9]) (va_list *, t_prtf *) =  {conv_c, conv_s, conv_p, conv_d, conv_i, conv_u, conv_x, conv_xx, conv_prc};
 	va_list args;
 	t_prtf struc;
-	int tmpIndex;
 
-	tmpIndex = 0;
 	innit_struct(&struc);
 	va_start(args, format);
 	while (format[struc.i])
@@ -27,9 +25,7 @@ int		ft_printf(const char *format, ...)
 		if (format[struc.i] == '%')
 		{
 			struc.i++;
-			tmpIndex = ft_checktype(format[struc.i]);
-			if (tmpIndex != -1)
-				(*tabFunc[tmpIndex]) (&args, &struc);
+			(*tabFunc[ft_checktype(format[struc.i])]) (&args, &struc);
 		}
 		else
 			ft_putchar(format[struc.i], &struc);
